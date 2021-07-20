af71b1464231db436b8718528056389a3a55fd85
robghys <Nalinnes1332!>
2021-07-20 10:11:42 +0200

    Bootstrapped some data (= categories) to our API.

--- src/main/java/guru/springfamework/domain/Category.java
+++ src/main/java/guru/springfamework/domain/Category.java
@@ -7,16 +7,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-/**
- * Created by jt on 9/24/17.
- */
 @Data
 @Entity
 public class Category {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
 }
