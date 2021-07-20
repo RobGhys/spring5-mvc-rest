671f095e9f9a2386c1dd1b8eb0d34fa059798a6e
robghys <Nalinnes1332!>
2021-07-20 12:12:42 +0200

    Created customer controller, DTO, mapper, repo, and service.

--- src/main/java/guru/springfamework/bootstrap/Bootstrap.java
+++ src/main/java/guru/springfamework/bootstrap/Bootstrap.java
@@ -1,23 +1,27 @@
 package guru.springfamework.bootstrap;
 
 import guru.springfamework.domain.Category;
+import guru.springfamework.domain.Customer;
 import guru.springfamework.repositories.CategoryRepository;
+import guru.springfamework.repositories.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Bootstrap implements CommandLineRunner {
     private CategoryRepository categoryRepository;
+    private CustomerRepository customerRepository;
 
-    /**
-     * Constructor
-     */
-    public Bootstrap(CategoryRepository categoryRepository) {
+    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository) {
         this.categoryRepository = categoryRepository;
+        this.customerRepository = customerRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        /********************************
+         ********* CATEGORIES **********
+         *******************************/
         Category fruits = new Category();
         fruits.setName("Fruits");
 
@@ -41,5 +45,27 @@ public class Bootstrap implements CommandLineRunner {
         categoryRepository.save(nuts);
 
         System.out.println("Loaded: " + categoryRepository.count() + " categories.");
+
+        /********************************
+         ********** CUSTOMERS ***********
+         *******************************/
+        Customer client1 = new Customer();
+        client1.setFirstName("Cli1");
+
+        Customer client2 = new Customer();
+        client2.setFirstName("Cli2");
+
+        Customer client3 = new Customer();
+        client3.setFirstName("Cli3");
+
+        Customer client4 = new Customer();
+        client4.setFirstName("Cli4");
+
+        customerRepository.save(client1);
+        customerRepository.save(client2);
+        customerRepository.save(client3);
+        customerRepository.save(client4);
+
+        System.out.println("Loaded: " + customerRepository.count() + " customers.");
     }
 }
